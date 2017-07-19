b3b9bef0e1912455619e732216997b5c089d2da1
reznov53 <adityawarman.fanaro@outlook.com>
2017-07-19 08:12:53 +0700

    Added curl & interface (WIP)

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+        <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <form action="/action_page.php" method="get">
+            Username: <input type="text" name="uname"><br>
+            Password: <input type="password" name="pword"><br>
+            <input type="submit" value="Submit">
+        </form> 
+    </body>
+</html>
